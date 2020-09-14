e505d6f2840cf452493030027968c3a31f2b2f67
jaewon Lee <jcircle67@gmail.com>
2020-09-14 10:18:46 +0900

    Update w2v_train.py
    fit part

--- CNN for Word2Vec/w2v_train.py
+++ CNN for Word2Vec/w2v_train.py
@@ -5,3 +5,7 @@ from gensim.models.word2vec import LineSentence, PathLineSentences
 model = Word2Vec.load("word2vec.model")
 score, predictions = model.wv.evaluate_word_analogies('./data/questions-words.txt')
 print(score)
+
+model = KeyedVectors.load_word2vec_format("./data/GoogleNews-vectorsnegative300.bin", binary=True , limit=60000)
+score, predictions = model.evaluate_word_analogies('./data/questions-words.txt')
+print(score)
